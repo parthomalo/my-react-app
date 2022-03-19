d84d23ee97d8d37aec101b28e9ebad4464070e54
partho2589 <parthomalo615@.com>
2022-03-19 16:39:46 +0600

    added all files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <a href="/home">Home</a>
+                <a href="/show">Show</a>
+                <a href="/about">About</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;

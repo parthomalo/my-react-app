54ee95460955a735b674333fa2265580168d3a99
partho2589 <parthomalo615@.com>
2023-03-15 20:39:37 +0600

    added darl mode data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import './App.css'
 import Dark from './components/Dark';
 import Home from './components/Home/Home';
 
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }; 
+  const darkModeStyle = {
+    backgroundColor: darkMode ? "black" : "white",
+    color: darkMode ? "white" : "black"
+  };
+
   return (
-    <div className="all-page">
+    <div  style={darkModeStyle} className={`App ${darkMode ? "dark-mode" : ""}`}>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
       <Dark></Dark>
      <Home/>
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import './Home.css'
 import Dark from '../Dark';
 import Header from '../Header/Header';
-
+import bag1 from '../../images/bags/bag-1.png'
+import bag2 from '../../images/bags/bag-2.png'
+import bag3 from '../../images/bags/bag-3.png'
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Dark></Dark>
+<h1 className='data'>This is home </h1>
+            <div class="card">
+                <img src={bag1} alt="Classic Black Leather Jacket"/>
+                    <h2>Classic Black Leather Jacket</h2>
+                    <p class="price">$249.99</p>
+                    <p>Made from high-quality leather, this classic black jacket is a wardrobe staple. Featuring a tailored fit, zippered pockets, and a sleek design, it's perfect for any occasion.</p>
+                    <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <img src={bag2} alt="Wireless Noise-Cancelling Headphones"/>
+                    <h2>Wireless Noise-Cancelling Headphones</h2>
+                    <p class="price">$179.99</p>
+                    <p>These wireless headphones use advanced noise-cancelling technology to provide crystal-clear sound. With a comfortable over-ear design and up to 30 hours of battery life, they're perfect for long listening sessions.</p>
+                    <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <img src={bag3} alt="Organic Cotton T-Shirt"/>
+                    <h2>Organic Cotton T-Shirt</h2>
+                    <p class="price">$29.99</p>
+                    <p>Made from 100% organic cotton, this t-shirt is soft, comfortable, and eco-friendly. It features a classic design and comes in a variety of colors, making it a versatile addition to any wardrobe.</p>
+                    <button>Add to Cart</button>
+            </div>
+
         </div>
     );
 };
